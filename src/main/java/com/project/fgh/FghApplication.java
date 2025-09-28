63d77a0ee7f32929f27d0eeb9f9ab10df028e28d
Geraldo Filho <geraldinhofilho14@gmail.com>
2025-09-28 18:04:13 -0300

    first commit

---
+++ src/main/java/com/project/fgh/FghApplication.java
@@ -0,0 +1,13 @@
+package com.project.fgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FghApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fgh/FghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fgh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
